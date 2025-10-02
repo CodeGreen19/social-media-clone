29837971c16a03920ac776afb9c431a6d4347792
shoubj-ahmed <codegreen19s@gmail.com>
2025-10-02 20:42:49 +0600

    feat:basic auth function is completed

--- src/features/auth/components/sign-in-form.tsx
+++ src/features/auth/components/sign-in-form.tsx
@@ -5,9 +5,13 @@ import { CustomFormWrapper } from "@/components/shared-form/custom-form-wrapper"
 import { Button } from "@/components/ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { authMutations } from "../mutations";
 import { signinSchema, SigninSchemaShape, SigninSchemaType } from "../schema";
+import { useRouter } from "next/navigation";
 
 export default function SignInForm() {
+  const router = useRouter();
+  const { mutate, isPending } = authMutations.signin();
   const form = useForm<SigninSchemaType>({
     resolver: zodResolver(signinSchema),
     defaultValues: {
@@ -16,7 +20,18 @@ export default function SignInForm() {
     },
   });
   return (
-    <CustomFormWrapper<SigninSchemaShape> form={form} onSubmit={() => {}}>
+    <CustomFormWrapper<SigninSchemaShape>
+      form={form}
+      onSubmit={(input) =>
+        mutate(input, {
+          onSuccess: (data) => {
+            if (data) {
+              router.push("/");
+            }
+          },
+        })
+      }
+    >
       <CustomFormField<SigninSchemaShape>
         form={form}
         input="email"
@@ -32,7 +47,9 @@ export default function SignInForm() {
         required
       />
 
-      <Button className="w-full">Submit</Button>
+      <Button disabled={isPending} type="submit">
+        Submit
+      </Button>
     </CustomFormWrapper>
   );
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import { db } from "@/drizzle/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  session: {
+    cookieCache: {
+      enabled: true,
+    },
+  },
+});
